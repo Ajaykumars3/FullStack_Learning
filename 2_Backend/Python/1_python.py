629487cb5f9be02557eb31ef8c4a73cc481d3267
Ajay Kumar S <94794025+Ajaykumars3@users.noreply.github.com>
2023-09-01 20:30:31 +0530

    Python Practiced folder added

---
+++ 2_Backend/Python/1_python.py
@@ -0,0 +1,30 @@
+
+x, y, z = 1, 2, 3
+num = [4, 5, 6]
+a, b, c = num
+l=m=n=5
+print(x , y , z)
+print(a , b , c)
+print(l , m , n)
+
+# variable name
+# myVar = 1
+# MyVar = 2
+# my_var = 3
+
+# x=3
+# y = str(4)
+# print(type(x))
+# print(type(y))
+
+# it's Single line comment
+""" its multi line comment"""
+
+# x=5
+# if x>3:
+#     print("True")
+# else:
+#     print("False")
+
+
+# print("hello")

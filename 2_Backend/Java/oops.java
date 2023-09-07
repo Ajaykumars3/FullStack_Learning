55524451b4e3ef1dd44d0109f6b309e39a63382d
Ajay Kumar S <94794025+Ajaykumars3@users.noreply.github.com>
2023-09-07 08:59:59 +0530

    oops

--- 2_Backend/Java/oops.java
+++ 2_Backend/Java/oops.java
@@ -1,5 +1,103 @@
 
 
+// Define an interface named "Shape" to represent geometric shapes
+interface Shape {
+    double area(); // Abstract method to calculate the area of the shape
+    void display(); // Abstract method to display information about the shape
+}
+
+// Create a concrete class "Circle" that implements the "Shape" interface
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Circle - Radius: " + radius);
+    }
+}
+
+// Create another concrete class "Rectangle" that implements the "Shape" interface
+class Rectangle implements Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Rectangle - Length: " + length + ", Width: " + width);
+    }
+}
+
+// Create an abstract class "ShapeContainer" to demonstrate dynamic method dispatch
+abstract class ShapeContainer {
+    protected Shape shape;
+
+    public ShapeContainer(Shape shape) {
+        this.shape = shape;
+    }
+
+    // Abstract method to display information about the shape
+    public abstract void display();
+}
+
+// Create a subclass "ShapeInfo" that extends "ShapeContainer"
+class ShapeInfo extends ShapeContainer {
+    public ShapeInfo(Shape shape) {
+        super(shape);
+    }
+
+    // Implement the display method to show information about the shape
+    @Override
+    public void display() {
+        shape.display();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Create instances of Circle and Rectangle
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+
+        // Demonstrate polymorphism with an array of Shape objects
+        Shape[] shapes = { circle, rectangle };
+
+        System.out.println("Calculating Areas and Displaying Shapes:");
+        for (Shape shape : shapes) {
+            System.out.println("Area: " + shape.area());
+            shape.display();
+            System.out.println();
+        }
+
+        // Demonstrate dynamic method dispatch using ShapeContainer
+        System.out.println("Dynamic Method Dispatch:");
+        ShapeContainer circleContainer = new ShapeInfo(circle);
+        ShapeContainer rectangleContainer = new ShapeInfo(rectangle);
+
+        circleContainer.display();
+        rectangleContainer.display();
+    }
+}
+
+
 
 // Define a class called "Person" to represent individuals
 class Person {

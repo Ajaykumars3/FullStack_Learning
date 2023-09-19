7ab9aefba9b3b7cb2126cd5e88ecf8640942d62c
Ajay Kumar S <94794025+Ajaykumars3@users.noreply.github.com>
2023-09-19 12:57:53 +0530

    oops

--- 2_Backend/Java/oops3.java
+++ 2_Backend/Java/oops3.java
@@ -1,3 +1,174 @@
+
+// Create an abstract class "AudioTrack" to represent audio tracks
+abstract class AudioTrack {
+    private String title;
+    private String artist;
+
+    public AudioTrack(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    // Abstract method to play the audio track
+    public abstract void play();
+
+    // Getter methods for title and artist
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+}
+
+// Create concrete classes for different types of audio tracks
+class Song extends AudioTrack {
+    private int duration; // Duration in seconds
+
+    public Song(String title, String artist, int duration) {
+        super(title, artist);
+        this.duration = duration;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing Song: " + getTitle());
+        System.out.println("Artist: " + getArtist());
+        System.out.println("Duration: " + duration + " seconds");
+    }
+}
+
+class Podcast extends AudioTrack {
+    private String host;
+
+    public Podcast(String title, String artist, String host) {
+        super(title, artist);
+        this.host = host;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing Podcast: " + getTitle());
+        System.out.println("Artist: " + getArtist());
+        System.out.println("Host: " + host);
+    }
+}
+
+// Create a class to simulate a music player
+class MusicPlayer {
+    public void playTrack(AudioTrack track) {
+        System.out.println("Now playing: " + track.getTitle());
+        track.play();
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Create instances of different audio tracks
+        AudioTrack song = new Song("Bohemian Rhapsody", "Queen", 360);
+        AudioTrack podcast = new Podcast("Java Tech Talk", "Tech Podcasts Inc.", "John Doe");
+
+        // Create a MusicPlayer object
+        MusicPlayer musicPlayer = new MusicPlayer();
+
+        // Play audio tracks
+        musicPlayer.playTrack(song);
+        musicPlayer.playTrack(podcast);
+    }
+}
+
+
+
+
+
+// Create an abstract class "Product" to represent products
+abstract class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Abstract method to display product information
+    public abstract void display();
+
+    // Getter methods for name and price
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+// Create concrete classes for different types of products
+class Electronics extends Product {
+    private String brand;
+
+    public Electronics(String name, double price, String brand) {
+        super(name, price);
+        this.brand = brand;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Electronics Product Name: " + getName());
+        System.out.println("Price: $" + getPrice());
+        System.out.println("Brand: " + brand);
+    }
+}
+
+class Clothing extends Product {
+    private String size;
+
+    public Clothing(String name, double price, String size) {
+        super(name, price);
+        this.size = size;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Clothing Product Name: " + getName());
+        System.out.println("Price: $" + getPrice());
+        System.out.println("Size: " + size);
+    }
+}
+
+// Create a class to simulate an online shopping system
+class OnlineStore {
+    public void addToCart(Product product) {
+        System.out.println("Adding to cart: " + product.getName());
+        product.display();
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Create instances of different products
+        Product electronicsProduct = new Electronics("Laptop", 999.99, "Dell");
+        Product clothingProduct = new Clothing("T-shirt", 19.99, "Large");
+
+        // Create an OnlineStore object
+        OnlineStore onlineStore = new OnlineStore();
+
+        // Add products to the shopping cart
+        onlineStore.addToCart(electronicsProduct);
+        onlineStore.addToCart(clothingProduct);
+    }
+}
+
+
+
+
+
+
+
 // Create an abstract class "Student" to represent students
 abstract class Student {
     private String name;

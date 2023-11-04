8b995ba63899c7880b8adb1296c52359491fd38f
Ajay Kumar S <94794025+Ajaykumars3@users.noreply.github.com>
2023-11-04 11:44:29 +0530

    python

--- 2_Backend/Python/1_python.py
+++ 2_Backend/Python/1_python.py
@@ -3,9 +3,9 @@ x, y, z = 1, 2, 3
 num = [4, 5, 6]
 a, b, c = num
 l=m=n=5
-print(x , y , z)
-print(a , b , c)
-print(l , m , n)
+# print(x , y , z)
+# print(a , b , c)
+# print(l , m , n)
 
 # variable name
 # myVar = 1
